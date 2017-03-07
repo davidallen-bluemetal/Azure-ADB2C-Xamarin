305912e463493378c22916e398b52337f96efb3a
David Allen <David Allen@DESKTOP-COEBE5P>
2017-03-07 13:05:25 -0600

    Add project files.

---
+++ B2CQuickstart/B2CQuickstart/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace B2CQuickstart
+{
+    public partial class App : Application
+    {
+        public static PublicClientApplication AuthenticationClient { get; set; }
+
+        // Step #1: Add Azure AD B2C tenant information.
+
+        // Tenant and Policies supplied in sample by Xamarin
+        /*
+        public static string ClientId = "8ca648fd-2dc3-4ff1-aa4c-e9bd5543f2d0";
+        public static string SignUpSignInPolicy = "B2C_1_SignInSignUp";
+        public static string Authority = "https://login.microsoftonline.com/momentsapp.onmicrosoft.com/";
+        */
+
+        // Test Tenant and Policies
+        public static string ClientId = "377cd8c6-a9df-4a55-aac5-8055475a550f";
+        public static string SignUpSignInPolicy = "B2C_1_DefaultSignInSignUp";
+        public static string Authority = "https://login.microsoftonline.com/BlueMetalElkay.onmicrosoft.com/";
+
+
+        public static string[] Scopes = { ClientId };
+
+        public App()
+        {
+            InitializeComponent();
+
+            AuthenticationClient = new PublicClientApplication(ClientId);
+
+            MainPage = new NavigationPage (new LoginPage());
+        }
+    }
+}
---
+++ B2CQuickstart/B2CQuickstart/LoginPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Identity.Client;
+using Xamarin.Forms;
+
+namespace B2CQuickstart
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // clear any tokens as if having logged out
+                if ((App.AuthenticationClient != null) && (App.AuthenticationClient.UserTokenCache.Count > 0) &&
+                    !string.IsNullOrWhiteSpace(App.AuthenticationClient.ClientId))
+                {
+                    App.AuthenticationClient.UserTokenCache.Clear(App.AuthenticationClient.ClientId);
+                }
+
+
+
+                // Step #2: Authenticate with Microsoft Authentication Library (MSAL).
+
+                // Authenticate users with Microsoft Authentication Library (MSAL).
+                var authenticationResult = await App.AuthenticationClient.AcquireTokenAsync(App.Scopes,
+                    "",
+                    UiOptions.SelectAccount,
+                    string.Empty,
+                    null,
+                    App.Authority,
+                    App.SignUpSignInPolicy);
+                // Navigate users into the main portion of our app.
+                await Navigation.PushAsync(new AuthenticationSuccessfulPage(authenticationResult));
+
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error Authenticating", ex.Message, "OK");
+            }
+        }
+    }
+}
